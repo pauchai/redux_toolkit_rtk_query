296d022fdf88d06df92203e2291913619f3acf02
Pauk <paul.khimyack@gmail.com>
2022-01-24 16:44:35 +0200

    simple example with count incrementing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import { useAppSelector } from './hooks/redux';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
 
 function App() { 
-    
+  const {count} = useAppSelector(state => state.userReducer)
+  const {increment} = userSlice.actions
+  const dispatch = useAppDispatch()
+  console.log(increment(5))
   return (
     <div >
-     
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment(1))}>INCREMENT</button>
     </div>
   );
 }
