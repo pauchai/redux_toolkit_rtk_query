187066a812b6c734114e3362094c8310301e450d
Pauk <paul.khimyack@gmail.com>
2022-01-25 11:34:16 +0200

    RTK query mutations - create

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,14 +1,21 @@
 import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
 import React from 'react';
+import { IPost } from '../models/IPost';
 import {postAPI} from "../services/PostService"
 import PostItem from './PostItem';
 
 const PostContainer = () => {
-    const {data:posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5)
-    console.log(error)
+    const {data:posts, error, isLoading} = postAPI.useFetchAllPostsQuery(15)
+
+    const [createPost, {error: createError, isLoading: createIsLoading} ] = postAPI.useCreatePostMutation()
+    const handleCreatePost = async () => {
+      const title = prompt()
+      await createPost({title, body:title} as IPost)
+    }
   return (
     <div>
         <div className="post__list">
+          <button onClick={handleCreatePost}>Add new post</button>
             {isLoading && <h1>Loading...</h1>}
             {error && <h1>{(error as FetchBaseQueryError).status}</h1>}
             {posts && posts.map(post => 
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -2,7 +2,8 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react"
 import { IPost } from "../models/IPost"
 export const postAPI = createApi({
     reducerPath: 'postAPI',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/'}),
+    tagTypes: ['Post'],
     endpoints: (build) => ({
         fetchAllPosts: build.query<IPost[], number>({
             query: (limit = 5) => ({
@@ -10,7 +11,17 @@ export const postAPI = createApi({
                 params: {
                     _limit: limit
                 }
-            })
+            }),
+            providesTags: result => ['Post']
+        }),
+        createPost: build.mutation<IPost, IPost>({
+            query: (post) => ({
+                url: `/posts`,
+                method: 'POST',
+                body: post
+
+            }),
+            invalidatesTags: ['Post']
         })
     })
 })
